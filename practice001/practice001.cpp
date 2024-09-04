efe6d167f123a19e79847b44778ea5c08815ffac
younsang1 <clark.youn@gmail.com>
2024-09-04 14:25:52 +0900

    Add files via upload

---
+++ practice001/practice001.cpp
@@ -0,0 +1,14 @@
+﻿// practice001.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    printf("과목   점수\n");
+    printf("==========\n");
+    printf("국어   100\n");
+    printf("수학   90\n");
+    printf("영어   80\n");
+    printf("==========\n")
+}
